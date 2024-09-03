1d9f49665fa09d89c859060027474d6eb3e925f6
Sergio Alejandro Ribera Costa <56278796+SergioRibera@users.noreply.github.com>
2024-09-03 14:46:00 -0400

    fix build cd

--- src/titles.py
+++ src/titles.py
@@ -9,7 +9,6 @@ CC_TEMPLATE_VARIABLE = "@TITLE_CC"
 TABLE_NAME_TEMPLATE_VARIABLE = "@TITLE_PROJECT_NAME"
 TABLE_STARS_TEMPLATE_VARIABLE = "@TITLE_PROJECT_STARS"
 TABLE_FORKS_TEMPLATE_VARIABLE = "@TITLE_PROJECT_FORKS"
-TABLE_ISSUES_TEMPLATE_VARIABLE = "@TITLE_PROJECT_ISSUES"
 TABLE_PR_TEMPLATE_VARIABLE = "@TITLE_PROJECT_PR"
 TABLE_LANG_TEMPLATE_VARIABLE = "@TITLE_PROJECT_LANG"
 
@@ -25,7 +24,6 @@ def replace_titles(data, lang: str, raw_temp: str) -> str:
     s = s.replace(TT_LIVE_TEMPLATE_VARIABLE, d['live'])
     s = s.replace(TABLE_STARS_TEMPLATE_VARIABLE, d['proj_title_stars'])
     s = s.replace(TABLE_FORKS_TEMPLATE_VARIABLE, d['proj_title_forks'])
-    s = s.replace(TABLE_ISSUES_TEMPLATE_VARIABLE, d['proj_title_issues'])
     s = s.replace(TABLE_PR_TEMPLATE_VARIABLE, d['proj_title_pr'])
     s = s.replace(TABLE_LANG_TEMPLATE_VARIABLE, d['proj_title_lang'])
     s = s.replace(TT_ABOUT_TEMPLATE_VARIABLE, d['about_title'])
